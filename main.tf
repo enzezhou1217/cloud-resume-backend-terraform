7e8aa0f2199eadb251c03dc03bdb912463d0926f
Enze Zhou <ZOO@EnzedeMacBook-Pro.local>
2023-02-04 21:50:03 +0800

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -262,8 +262,8 @@ resource "aws_route53_record" "ipv4" {
   type    = "A"
 
   alias {
-    name                   = aws_cloudfront_distribution.s3_distribution.dns_name
-    zone_id                = aws_cloudfront_distribution.s3_distribution.zone_id
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
     evaluate_target_health = true
   }
 }
