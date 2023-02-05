7ede5266b8e260c6255d57d4868fa1377af815ba
Enze Zhou <ZOO@EnzedeMacBook-Pro.local>
2023-02-05 13:31:38 +0800

    update lambda

--- main.tf
+++ main.tf
@@ -81,13 +81,8 @@ resource "aws_iam_role_policy" "dynamodb-lambda-policy" {
    })
 }
 
-data "archive_file" "zip_the_python_code" {
-  type        = "zip"
-  source_dir  = "/python/"
-  output_path = "/python/cloud-resume-lambda.zip"
-}
 resource "aws_lambda_function" "cloud-resume-lambda" {
-  filename         = "/python/cloud-resume-lambda.zip"
+  filename         = "cloud-resume-lambda.zip"
   function_name    = "cloud-resume-lambda"
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "index.lambda_handler"
